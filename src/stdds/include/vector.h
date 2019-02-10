876a670802cf3e1fdeafc2dc823450e94d6c6f87
Hendrik <borchehq@hu-berlin.de>
2019-02-10 11:31:52 +0100

    Add at() and pop() and change definition of get() and get_last().

--- src/stdds/include/vector.h
+++ src/stdds/include/vector.h
@@ -61,11 +61,58 @@ inline void delete_vector(vector *vec)
   free(vec->data);
 }
 
+inline void *at(vector *vec, size_t index)
+{
+  if(index >= vec->occupied)
+    {
+      return NULL;
+    }
+  return &vec->data[vec->size_element * index];
+}
+
+/**
+*   @brief Removes the element at the specified index and if applicable
+*   calls delete_datatype().
+*   @return Returns -1 if no memory, -2 if illegal index and 0 on success.
+*/
+inline int rem(vector *vec, size_t index)
+{
+  size_t offset;
+  void *tmp = vec->data;
+  // Check whether index is out of bounds.
+  if(index >= vec->occupied)
+  {
+    return -2;
+  }
+  offset = vec->occupied - 1 - index;
+  if(vec->delete_datatype != NULL)
+  {
+    vec->delete_datatype(at(vec, index));
+  }
+  // Close the gap if not the last element.
+  if(index < vec->occupied - 1)
+  {
+    memmove(&vec->data[index * vec->size_element],
+            &vec->data[(index + 1) * vec->size_element],
+            offset * vec->size_element);
+  }
+  vec->occupied--;
+  if(vec->allocated >= 2 * MIN_CAP && vec->allocated >= 2 * vec->occupied)
+  {
+    tmp = realloc(tmp, (vec->allocated / 2) * vec->size_element);
+    if(tmp != NULL){
+      vec->data = tmp;
+      vec->allocated /= 2;
+    }
+  }
+  return 0;
+}
+
 /**
 *   @brief Appends a new element to the end of the vector.
 *   @return Returns -1 if no memory is available and 0 on success.
 */
-inline int append(vector *vec, const void *element)
+inline int push(vector *vec, void *element)
 {
   byte_t *tmp = vec->data;
   if(vec->allocated == vec->occupied)
@@ -87,13 +134,27 @@ inline int append(vector *vec, const void *element)
   return 0;
 }
 
+inline void *pop(vector *vec)
+{
+  void *elem = &vec->data[(vec->occupied - 1) * vec->size_element];
+  void *ret = malloc(vec->size_element);
+  if(ret == NULL)
+  {
+    return NULL;
+  }
+  memcpy(ret, elem, vec->size_element);
+  // Delete elem.
+  rem(vec, vec->size_element - 1);
+  return ret;
+}
+
 /**
 *   @brief Inserts the specified element at the specified position in this vec
 *   and shifts the element currently at that position and any subsequent
 *   elements to the right.
 *   @return Returns -1 on failure and 0 on success.
 */
-inline int insert(vector *vec, const void *element, size_t index)
+inline int insert(vector *vec, void *element, size_t index)
 {
   byte_t *tmp = vec->data;
   if(vec->allocated == vec->occupied)
@@ -131,20 +192,27 @@ inline int insert(vector *vec, const void *element, size_t index)
 *   @brief Retrieves the element at the specified index.
 *   @return The corresponding element on success and NULL on error.
 */
-inline void *get(const vector *vec, size_t index)
+inline void *get(vector *vec, size_t index)
 {
   if(index >= vec->occupied)
   {
     return NULL;
   }
-  return &vec->data[vec->size_element * index];
+  void *elem = &vec->data[vec->size_element * index];
+  void *ret = malloc(vec->size_element);
+  if(ret == NULL)
+  {
+    return NULL;
+  }
+  memcpy(ret, elem, vec->size_element);
+  return ret;
 }
 
 /**
 *   @brief Sets a new element at the specified index.
 *   @return Returns -1 if illegal index and 0 on success.
 */
-inline int set(vector *vec, const void *element, size_t index)
+inline int set(vector *vec, void *element, size_t index)
 {
   if(index >= vec->occupied)
   {
@@ -156,53 +224,18 @@ inline int set(vector *vec, const void *element, size_t index)
 }
 
 /**
-*   @brief Removes the element at the specified index and if applicable
-*   calls delete_datatype().
-*   @return Returns -1 if no memory, -2 if illegal index and 0 on success.
-*/
-inline int rem(vector *vec, size_t index)
-{
-  size_t offset;
-  void *tmp = vec->data;
-  // Check whether index is out of bounds.
-  if(index >= vec->occupied)
-  {
-    return -2;
-  }
-  offset = vec->occupied - 1 - index;
-  if(vec->delete_datatype != NULL)
-  {
-    vec->delete_datatype(get(vec, index));
-  }
-  // Close the gap if not the last element.
-  if(index < vec->occupied - 1)
-  {
-    memmove(&vec->data[index * vec->size_element],
-            &vec->data[(index + 1) * vec->size_element],
-            offset * vec->size_element);
-  }
-  vec->occupied--;
-  if(vec->allocated >= 2 * MIN_CAP && vec->allocated >= 2 * vec->occupied)
-  {
-    tmp = realloc(tmp, (vec->allocated / 2) * vec->size_element);
-    if(tmp != NULL){
-      vec->data = tmp;
-      vec->allocated /= 2;
-    }
-    else
-    {
-      return -1;
-    }
-  }
-  return 0;
-}
-
-/**
 *   @brief returns the last element in the vec
 **/
-inline void *get_last(const vector *vec)
+inline void *get_last(vector *vec)
 {
-    return &vec->data[(vec->occupied - 1) * vec->size_element];
+  void *elem = &vec->data[(vec->occupied - 1) * vec->size_element];
+  void *ret = malloc(vec->size_element);
+  if(ret == NULL)
+  {
+    return NULL;
+  }
+  memcpy(ret, elem, vec->size_element);
+  return ret;
 }
 
 /**
--- src/stdds/src/vector.c
+++ src/stdds/src/vector.c
@@ -2,17 +2,19 @@
 
 extern int new_vector(vector *vec, size_t size_element,
 size_t initial_size, void (*delete_datatype)(void *data));
-extern int append(vector *vec, const void *element);
+extern int push(vector *vec, void *element);
+inline void *pop(vector *vec);
 extern void delete_vector(vector *vec);
-extern int insert(vector *vec, const void *element, size_t index);
-extern void *get(const vector *vec, size_t index);
-extern int set(vector *vec, const void *element, size_t index);
+extern int insert(vector *vec, void *element, size_t index);
+extern void *get(vector *vec, size_t index);
+extern int set(vector *vec, void *element, size_t index);
 extern int rem(vector *vec, size_t index);
-extern void *get_last(const vector *vec);
+extern void *get_last(vector *vec);
 extern size_t size(const vector *vec);
 extern void *to_array(const vector *vec);
+void *at(vector *vec, size_t index);
 
-void sortv(vector *vec, int (*compar)(const void *, const void*))
+void sortv(vector *vec, int (*compar)(const void *, const void *))
 {
   qsort(vec->data, vec->occupied, vec->size_element, compar);
 }
