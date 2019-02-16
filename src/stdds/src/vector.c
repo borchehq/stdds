76fa446e49be0d1e9e88b1b422e335ee7e5000e6
Hendrik <borchehq@hu-berlin.de>
2019-02-16 12:07:19 +0100

    changed inline to extern. Make sortv() extern.

--- src/stdds/src/vector.c
+++ src/stdds/src/vector.c
@@ -3,7 +3,7 @@
 extern int new_vector(vector *vec, size_t size_element,
 size_t initial_size, void (*delete_datatype)(void *data));
 extern int push(vector *vec, void *element);
-inline void *pop(vector *vec);
+extern void *pop(vector *vec);
 extern void delete_vector(vector *vec);
 extern int insert(vector *vec, void *element, size_t index);
 extern void *get(vector *vec, size_t index);
@@ -18,8 +18,4 @@ extern int split(vector *vec, vector *res, size_t index);
 extern bool is_empty(vector *vec);
 extern bool index_valid(vector *vec, size_t index);
 extern int clone(vector *const vec, vector *res);
-
-void sortv(vector *vec, int (*compar)(const void *, const void *))
-{
-  qsort(vec->data, vec->occupied, vec->size_element, compar);
-}
+extern void sortv(vector *vec, int (*compar)(const void *, const void *));
