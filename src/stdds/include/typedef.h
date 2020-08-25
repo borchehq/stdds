44b47116a73fe4cc372474c16a27285456a13dbd
Hendrik <borchehq@hu-berlin.de>
2020-08-25 09:43:37 +0200

    Move shared struct definitions and typedefs into separate header file.

---
+++ src/stdds/include/typedef.h
@@ -0,0 +1,12 @@
+#ifndef TYPEDEF_FDT_H
+#define TYPEDEF_FDT_H
+
+typedef unsigned char byte_t;
+typedef struct type_conf_s dsconf;
+
+struct type_conf_s
+{
+  int (*clone_ds)(void *data, void *res);
+  void (*delete_ds)(void *data);
+};
+#endif
